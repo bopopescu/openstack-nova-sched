a71ff01bb75e0c1e675bd5b92e6aa354887d67d2
Arunpn <owlpostarun@gmail.com>
2013-04-24 03:40:12 -0700

    Direct Scheduler code is added

---
+++ nova/nova/scheduler/direct_scheduler.py
@@ -0,0 +1,124 @@
+# Copyright (c) 2011 OpenStack Foundation
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""
+Direct Scheduler: 
+Given a hostname, this scheduler looks through the available list of hosts and schedules the VM on the host which matches the hostname
+Sets the status Error if host not found
+
+Usage:
+nova boot [arguments] --meta hostname=<hostname>
+
+-Arunpn
+"""
+
+import random
+
+from oslo.config import cfg
+
+from nova.compute import instance_types
+from nova import exception
+from nova.openstack.common import log as logging
+from nova.openstack.common.notifier import api as notifier
+from nova.scheduler import driver
+from nova.scheduler import scheduler_options
+
+CONF = cfg.CONF
+LOG = logging.getLogger(__name__)
+
+
+#FLAGS = flags.FLAGS
+#LOG = logging.getLogger(__name__)
+
+
+class FilterScheduler(driver.Scheduler):
+    """Implements Scheduler as a direct node selector."""
+
+    def _filter_hosts(self, request_spec, hosts, filter_properties):
+        """Filter a list of hosts based on hostname."""
+        #return the compute host that matches the request
+        ignore_hosts = filter_properties.get('ignore_hosts', [])
+        msg = _("Direct filtering is being done")
+        metadata = request_spec['instance_properties']['metadata']
+        hostname = metadata['hostname']
+        msg = _("Hostname extracted from metadata dict is:")
+        msg = _(hostname)
+        hosts = [host for host in hosts if host == hostname]
+        #hosts = [host for host in hosts if host not in ignore_hosts]
+        msg = _("Compute hosts that are being returned by the filter:")
+        msg = _(hosts)
+        return hosts
+
+    def _schedule(self, context, topic, request_spec, filter_properties):
+        """Returns the host which corresponds to the hostname passed. If there are multiple hosts by the same name, return a random host from the matches"""
+
+        elevated = context.elevated()
+        hosts = self.hosts_up(elevated, topic)
+        if not hosts:
+            msg = _("Is the appropriate service running?")
+            raise exception.NoValidHost(reason=msg)
+
+        hosts = self._filter_hosts(request_spec, hosts, filter_properties)
+        if not hosts:
+            msg = _("Could not find another compute")
+            raise exception.NoValidHost(reason=msg)
+
+        return hosts[int(random.random() * len(hosts))]
+
+    def select_hosts(self, context, request_spec, filter_properties):
+        """Selects a set of random hosts."""
+        return [self._schedule(context, CONF.compute_topic,
+            request_spec, filter_properties)
+            for instance_uuid in request_spec.get('instance_uuids', [])]
+
+    def schedule_run_instance(self, context, request_spec,
+                              admin_password, injected_files,
+                              requested_networks, is_first_time,
+                              filter_properties):
+        """Create and run an instance or instances."""
+        instance_uuids = request_spec.get('instance_uuids')
+        for num, instance_uuid in enumerate(instance_uuids):
+            request_spec['instance_properties']['launch_index'] = num
+            try:
+                host = self._schedule(context, CONF.compute_topic,
+                                      request_spec, filter_properties)
+                if not host:
+                    msg = _("Direct Scheduler ERROR: No compute host exists by that hostname")
+                else:
+                    updated_instance = driver.instance_update_db(context,
+                            instance_uuid)
+                    self.compute_rpcapi.run_instance(context,
+                            instance=updated_instance, host=host,
+                            requested_networks=requested_networks,
+                            injected_files=injected_files,
+                            admin_password=admin_password,
+                            is_first_time=is_first_time,
+                            request_spec=request_spec,
+                            filter_properties=filter_properties)
+            except Exception as ex:
+                # NOTE(vish): we don't reraise the exception here to make sure
+                #             that all instances in the request get set to
+                #             error properly
+                driver.handle_schedule_error(context, ex, instance_uuid,
+                                             request_spec)
+
+    def schedule_prep_resize(self, context, image, request_spec,
+                             filter_properties, instance, instance_type,
+                             reservations):
+        """Select a target for resize."""
+        host = self._schedule(context, CONF.compute_topic, request_spec,
+                              filter_properties)
+        self.compute_rpcapi.prep_resize(context, image, instance,
+                instance_type, host, reservations)
